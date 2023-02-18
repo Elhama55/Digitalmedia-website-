aa4c3183d58d0c33e99ffc613b6033a3f32a180e
Elham <97202863+Elhama55@users.noreply.github.com>
2023-02-18 18:48:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// "use strict";
+
+// add events on multiple elements
+
+const addEventOnElement = function (elements, eventType, callback) {
+	for (let i = 0, len = elements.length; i < len; i++) {
+		elements[i].addEventListener(eventType, callback);
+	}
+};
+
+// navbar
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+	navbar.classList.toggle("active");
+	navToggleBtn.classList.toggle("active");
+	overlay.classList.toggle("active");
+	document.body.classList.toggle("nav-active");
+};
+addEventOnElement([navToggleBtn, overlay], "click", toggleNavbar);
+
+// parallex effect
+
+const parallexElements = document.querySelectorAll("[data-parallax]");
+
+window.addEventListener("mousemove", (event) => {
+	for (let i = 0, len = parallexElements.length; i < len; i++) {
+		const movementX =
+			(event.clientX / window.innerWidth) *
+			Number(parallexElements[i].dataset.parallaxSpeed);
+		const movmentY =
+			(event.clientY / window.innerHeight) *
+			Number(parallexElements[i].dataset.parallaxSpeed);
+
+		parallexElements[i].animate(
+			{
+				transform: `translate(${movementX}px, ${movmentY}px)`,
+			},
+			{ duration: 500, fill: "forwards" }
+		);
+	}
+});
